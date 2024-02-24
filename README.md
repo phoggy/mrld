41e6cfb7411b7a450133bb1ae246e811427c5bc6
phynix <phynix@phoggy.me>
2024-02-24 10:12:39 -0800

    first working

---
+++ README.md
@@ -0,0 +1,88 @@
+# Password Strength Estimate
+
+A command-line tool that uses [zxcvbn](https://github.com/shssoichiro/zxcvbn-rs) to report the estimated strength of a 
+password/phrase, with the goal of encouraging or enforcing use of strong ones. 
+
+A simplified form suitable for display during input is reported by default, following the [bitwarden](https://bitwarden.com/password-strength/) model:
+- map the 0-4 score value to an adjective: < 2 = `very weak`, 2 =`weak`, 3 =`good`, 4 =`strong`
+- use only the 10k/s 'offline attack, slow hash, many cores' crack time.
+
+
+```bash
+$ echo "my password" | emrld
+very weak, 11 seconds to crack
+```
+The adjective is *colored* according to the score by default:
+- < 2 = red
+- 2 = yellow
+- 3 = blue
+- 4 = green (mrld)
+                                                 
+```bash
+Options:
+  -p, --pretty      split output on multiple lines
+  -s, --score       add score as an integer from 0 to 4
+  -n, --no-color    do not use color
+  -t, --terse       minimize output
+  -v, --verbose     output complete estimate as JSON
+  --version         output version information and exit
+  --help            display usage information
+```
+                               
+
+The `--verbose` option can be used to report all estimate data as JSON, e.g.
+
+```bash
+$ echo "my password" | emrld --verbose --pretty
+{
+  "guesses": 110000,
+  "guesses_log10": 5.041392685158225,
+  "crack_times": {
+    "100_per_hour": "1 month",
+    "10_per_second": "3 hours",
+    "10k_per_second": "11 seconds",
+    "10B_per_second": "less than a second"
+  },
+  "score": 1,
+  "feedback": {
+    "warning": "ThisIsSimilarToACommonlyUsedPassword",
+    "suggestions": [
+      "AddAnotherWordOrTwo"
+    ]
+  },
+  "sequence": [
+    {
+      "i": 0,
+      "j": 2,
+      "token": "my ",
+      "pattern": "bruteforce",
+      "guesses": 1000
+    },
+    {
+      "i": 3,
+      "j": 10,
+      "token": "password",
+      "pattern": "dictionary",
+      "matched_word": "password",
+      "rank": 2,
+      "dictionary_name": "Passwords",
+      "reversed": false,
+      "l33t": false,
+      "sub": null,
+      "sub_display": null,
+      "uppercase_variations": 1,
+      "l33t_variations": 1,
+      "base_guesses": 2,
+      "guesses": 50
+    }
+  ],
+  "calc_time": {
+    "secs": 0,
+    "nanos": 118927000
+  }
+}
+```
+
+## Installation
+
+Check this project's GitHub Releases page for binaries.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,145 @@
-fn main() {
-    println!("Hello, world!");
+use ansi_term::Colour::{Blue, Green, Red, Yellow};
+use argh::FromArgs;
+use serde_json;
+use std::io::{self, BufRead};
+use serde_json::json;
+use zxcvbn::{zxcvbn, Entropy};
+
+const NAME: &'static str = "emrld";
+const VERSION: &'static str = "0.1.0";
+
+#[derive(FromArgs)]
+/// A password/phrase strength estimator that, by default, outputs a simplified format
+/// suitable for displaying alongside password/phrase input. The password/phrase is read
+/// from stdin.
+struct Args {
+    /// split output on multiple lines
+    #[argh(switch, long = "pretty", short = 'p')]
+    multiline: bool,
+
+    /// add score as an integer from 0 to 4
+    #[argh(switch, long = "score", short = 's')]
+    score: bool,
+
+    /// do not use color
+    #[argh(switch, long = "no-color", short = 'n')]
+    plain: bool,
+
+    /// minimize output
+    #[argh(switch, long = "terse", short = 't')]
+    terse: bool,
+
+    /// output complete estimate as JSON
+    #[argh(switch, long = "verbose", short = 'v')]
+    verbose: bool,
+
+    /// output version information and exit
+    #[argh(switch, long = "version")]
+    version: bool,
+}
+
+fn main() -> Result<(), String> {
+    let args: Args = argh::from_env();
+    return if args.version {
+        version()
+    } else {
+        estimate(args)
+    };
+}
+
+fn estimate(args: Args) -> Result<(), String> {
+    let stdin = io::stdin();
+    let pass = stdin.lock().lines().next().unwrap().unwrap();
+    return match zxcvbn(&pass, &[]) {
+        Ok(estimate) => {
+            print(args, estimate);
+            Ok(())
+        }
+        Err(e) => Err(format!("Error estimating strength: {}", e)),
+    };
+}
+
+fn print(args: Args, estimate: Entropy) {
+    if args.verbose {
+        print_verbose(args, estimate);
+    } else {
+        print_summary(args, estimate);
+    }
+}
+
+fn print_verbose(args: Args, estimate: Entropy) {
+    let mut json = serde_json::to_value(&estimate).expect("json serialization failed");
+
+    // Edit result to replace "crack_times":{"guesses":110000} with calculated values
+
+    let times = estimate.crack_times();
+    let update = json!({
+        "100_per_hour": times.online_throttling_100_per_hour().to_string(),
+        "10_per_second": times.online_no_throttling_10_per_second().to_string(),
+        "10k_per_second": times.offline_slow_hashing_1e4_per_second().to_string(),
+        "10B_per_second": times.offline_fast_hashing_1e10_per_second().to_string()
+    });
+    *json.get_mut("crack_times").unwrap() = update;
+
+    if args.multiline {
+        println!("{}", serde_json::to_string_pretty(&json).unwrap());
+    } else {
+        println!("{}", serde_json::to_string(&json).unwrap());
+    }
+}
+
+fn print_summary(args: Args, estimate: Entropy) {
+    let crack_time = estimate.crack_times().offline_slow_hashing_1e4_per_second();
+    let score = estimate.score();
+    let mut separator = ", ";
+    let mut prefix = String::new();
+    let mut suffix = " to crack";
+    let color;
+    let name;
+    let label;
+
+    match score {
+        0 | 1 => {
+            color = Red.normal();
+            name = "very weak"
+        }
+        2 => {
+            color = Yellow.normal();
+            name = "weak"
+        }
+        3 => {
+            color = Blue.normal();
+            name = "good"
+        }
+        4 => {
+            color = Green.bold();
+            name = "strong"
+        }
+        _ => panic!("unknown score value"),
+    };
+
+    if args.plain {
+        label = name.to_string();
+    } else {
+        label = color.paint(name).to_string();
+    }
+    if args.multiline {
+        separator = "\n"
+    }
+    if args.terse {
+        if !args.multiline {
+            separator = ","
+        }
+        suffix = ""
+    }
+    if args.score {
+        prefix = format!("{}{}", score, separator);
+    }
+
+    println!("{}{}{}{}{}", prefix, label, separator, crack_time, suffix);
+}
+
+fn version() -> Result<(), String> {
+    println!("{} {}", NAME, VERSION);
+    Ok(())
 }
