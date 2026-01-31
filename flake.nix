040a52a2c171706ad118b1298c332c612d01e882
phynix <phynix@phoggy.me>
2026-01-31 11:32:04 -0800

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "mrld - Password strength evaluator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "mrld";
+            version = "0.1.0";
+            src = self;
+            cargoHash = "sha256-8IKe8Ps//m3yvG5EQjA/DadZ9mdmuoW7MA6DgMMLrdU=";
+            meta = with pkgs.lib; {
+              description = "Password strength evaluator";
+              homepage = "https://github.com/phoggy/mrld";
+              license = licenses.gpl3Only;
+            };
+          };
+        };
+      }
+    );
+}
